cc140cb4ba4f35a67d3959ecdd52513cb377ed4d
norole159 <norman.olejnik@alumni.fh-aachen.de>
2020-01-22 15:07:27 +0100

    neues Interface; funktionen in JS implementiert

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = "expires=" + d.toUTCString();
-  index.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+/*
+const { session } = require("electron");
+//ElectronCookies.enable({ origin: "https://example.com" });
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = index.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+// Durchsuche all Cookies.
+session.defaultSession.cookies
+  .get({})
+  .then(cookies => {
+    console.log(cookies);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// Query all cookies associated with a specific url.
+session.defaultSession.cookies
+  .get({ url: "http://www.index.html" })
+  .then(cookies => {
+    console.log(cookies);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// Set a cookie with the given cookie data;
+// may overwrite equivalent cookies if they exist.
+const cookie = {
+  url: "http://www.index.html",
+  name: "dummy_name",
+  value: "dummy"
+};
+
+session.defaultSession.cookies.set(cookie).then(
+  () => {
+    console.log(cookies);
+  },
+  error => {
+    console.error(error);
   }
-  return "";
-}
-
-function checkCookie() {
-  var user = getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-    user = prompt("Please enter your name:", "");
-    if (user != "" && user != null) {
-      setCookie("username", user, 365);
-    }
-  }
-}
-
+);
+*/
 /*
 var los = document.getElementById("los");
-los.addEventListener("click", alert("funktioniert!"), true);
+los.addEventListener("click", true, alert("funktioniert!"));
 */
+
+var buttomTest = document.getElementById("los");
+buttomTest.addEventListener("mouseover", function() {
+  alert("MOUSEOVER");
+});
+
+function init() {
+  var tab = document.getElementById("tabpanel");
+  tab.addEventListener("click", clickHandler);
+}
+
+function clickHandler(elem) {
+  var target = elem.target;
+
+  var selectedTab = document.querySelector('[aria-selected="true"]');
+  selectedTab.setAttribute("aria-selected", false);
+  target.setAttribute("aria-selected", true);
+
+  var panels = document.querySelector('[aria-hidden="false"]');
+  panels.setAttribute("aria-hidden", true);
+
+  var panelId = target.getAttribute("aria-controls"),
+    panel = document.getElementById(panelId);
+  panel.setAttribute("aria-hidden", false);
+}
